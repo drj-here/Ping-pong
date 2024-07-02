996d50e0bd57067a0efc9e555532c76d2b8645fd
drj-here <rajdishu2003@gmail.com>
2024-07-02 11:04:15 +0530

    Added moving feature to paddle

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // so that js executes when the content is loaded
 document.addEventListener("DOMContentLoaded",()=>{
     let table=document.getElementById("ping-pong-table");
+    let paddle=document.getElementById("paddle");
    let ball=document.getElementById("ball");
    let ballX=50;
    let ballY=50;
@@ -23,4 +24,16 @@ document.addEventListener("DOMContentLoaded",()=>{
     if(ballX>=table.offsetWidth-ball.offsetWidth || ballX<=0) dx*=-1;
     if(ballY>=table.offsetHeight-ball.offsetHeight || ballY<=0) dy*=-1;
    },1);
+
+   let paddleY=0;
+   let pdy=5;
+   document.addEventListener("keydown",(event)=>{
+    if(event.keyCode==38){
+        if(paddleY>pdy) paddleY+=(-1*pdy);
+    }
+    else if(event.keyCode==40 && paddleY<table.offsetHeight-paddle.offsetHeight-pdy) { paddleY+=pdy;}
+
+    paddle.style.top=`${paddleY}px`;
+    console.log(paddleY)
+   })
 });
